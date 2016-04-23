b015f7eab49b6c65741b23664a929d6b4eccbba6
Eric P <proulx712@gmail.com>
2016-04-23 10:53:00 -0700

    more html changes

--- marketing_program_v2/leads/models.py
+++ marketing_program_v2/leads/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+# @python_2_unicode_compatible
+# class LeadFields():
+#     id = models.IntegerField("id")
+#     displayName = models.CharField("displayName", blank=True, max_length=255)
+#     dataType = models.CharField("dataType", blank=True, max_length=255)
+#     length = models.IntegerField("length")
+#
+#     def __str__(self):
+#         return self.dataType
+#
+#     def get_absolute_url(self):
+#         return reverse('leads:detail', kwargs={'dataTyoe': self.dataType})
