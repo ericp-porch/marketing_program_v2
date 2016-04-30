a63098d13971eb68a1b62da2693e440c3539f07f
kharmon11 <kharmon11@gmail.com>
2016-04-30 09:18:28 -0700

    some things

--- marketing_program_v2/leads/models.py
+++ marketing_program_v2/leads/models.py
@@ -5,15 +5,15 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
-# @python_2_unicode_compatible
-# class LeadFields():
-#     id = models.IntegerField("id")
-#     displayName = models.CharField("displayName", blank=True, max_length=255)
-#     dataType = models.CharField("dataType", blank=True, max_length=255)
-#     length = models.IntegerField("length")
-#
-#     def __str__(self):
-#         return self.dataType
-#
-#     def get_absolute_url(self):
-#         return reverse('leads:detail', kwargs={'dataTyoe': self.dataType})
+@python_2_unicode_compatible
+class LeadFields():
+    id = models.IntegerField("id")
+    displayName = models.CharField("displayName", blank=True, max_length=255)
+    dataType = models.CharField("dataType", blank=True, max_length=255)
+    length = models.IntegerField("length")
+
+    def __str__(self):
+        return self.dataType
+
+    def get_absolute_url(self):
+        return reverse('leads:detail', kwargs={'dataType': self.dataType})
--- marketing_program_v2/leads/views.py
+++ marketing_program_v2/leads/views.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from .models import LeadFields
 
 
 class AboutView(TemplateView):
@@ -10,5 +11,19 @@ class AboutView(TemplateView):
 
 class LeadView(TemplateView):
     template_name = "leads/view.html"
+    # LIVE VERSION
+    entries = LeadFields.objects.all()
+    def get(self, request):
+        return render(request, self.template_name, {'fields': entries})
+    def post(self, request):
+        result = request.POST.getlist('fields')
+        result_dict = {}
+        for row in result:
+            array_f = [x.strip() for x in row.split(',')]
+            result_dict[array_f[0]] = array_f[1]
+        json_response = get_leads(result_dict.values())
+        return JsonResponse(json_response, safe=False)
 # def leadview(request):
 #     return render(request, 'leads/view.html')
+
+
