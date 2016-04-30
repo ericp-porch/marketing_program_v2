26c4bf8c677d265198e325884b05314c5f53ffec
Eric P <proulx712@gmail.com>
2016-04-30 09:29:38 -0700

    fixed what ken broke

--- marketing_program_v2/leads/views.py
+++ marketing_program_v2/leads/views.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 from .models import LeadFields
 
 
@@ -11,19 +12,20 @@ class AboutView(TemplateView):
 
 class LeadView(TemplateView):
     template_name = "leads/view.html"
-    # LIVE VERSION
-    entries = LeadFields.objects.all()
+
     def get(self, request):
+        entries = LeadFields.objects.all()
         return render(request, self.template_name, {'fields': entries})
+
     def post(self, request):
-        result = request.POST.getlist('fields')
-        result_dict = {}
-        for row in result:
-            array_f = [x.strip() for x in row.split(',')]
-            result_dict[array_f[0]] = array_f[1]
-        json_response = get_leads(result_dict.values())
-        return JsonResponse(json_response, safe=False)
+        # result = request.POST.getlist('fields')
+        # result_dict = {}
+        # for row in result:
+        #     array_f = [x.strip() for x in row.split(',')]
+        #     result_dict[array_f[0]] = array_f[1]
+        # json_response = get_leads(result_dict.values())
+        # return JsonResponse(json_response, safe=False)
+        return render(request,"post method")
+
 # def leadview(request):
 #     return render(request, 'leads/view.html')
-
-
