316041cc15e3b1cd92849cc1fa7906d690090307
Eric P <proulx712@gmail.com>
2016-04-30 21:03:30 -0700

    updated models stuff

--- marketing_program_v2/leads/models.py
+++ marketing_program_v2/leads/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Fields(models.Model):
     id = models.IntegerField("id", primary_key=True)
-    displayName = models.CharField("displayName", max_length=255)
-    dataType = models.CharField("dataType", max_length=255)
+    display_name = models.CharField("display_name", max_length=255)
+    data_type = models.CharField("data_type", max_length=255)
     length = models.IntegerField("length")
-    rest_name = models.CharField("restName", max_length=255)
-    rest_read_only = models.NullBooleanField("restReadOnly")
+    rest_name = models.CharField("rest_name", max_length=255)
+    rest_read_only = models.NullBooleanField("rest_read_only")
     soap_name = models.CharField("soapName", max_length=255)
-    soap_read_only = models.NullBooleanField("soapReadOnly")
+    soap_read_only = models.NullBooleanField("soap_read_only")
 
     class Meta:
         db_table = "fields"
@@ -20,10 +20,10 @@ class Fields(models.Model):
 class Leads(models.Model):
     id = models.IntegerField("id", primary_key=True)
     email = models.CharField("email", max_length=255, null=True)
-    updated_at = models.DateTimeField("updatedAt", null=True)
-    created_at = models.DateTimeField("createdAt", null=True)
-    last_name = models.CharField("lastName", max_length=255, null=True)
-    first_name = models.CharField("firstName", max_length=255, null=True)
+    updated_at = models.DateTimeField("updated_at", null=True)
+    created_at = models.DateTimeField("created_at", null=True)
+    last_name = models.CharField("last_name", max_length=255, null=True)
+    first_name = models.CharField("first_name", max_length=255, null=True)
     document = JSONField("document", null=True)
 
     class Meta:
