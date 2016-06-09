2e91bfc29d68e9eee02db42f7ea3b77bf5477705
kharmon11 <kharmon11@gmail.com>
2016-06-08 17:15:16 -0700

    Split filterview into multiple methods. Some cleanup. CSV now downloaded by browser

--- marketing_program_v2/leads/views.py
+++ marketing_program_v2/leads/views.py
@@ -6,6 +6,7 @@ import csv
 from django.http import HttpResponse
 from django.utils.encoding import smart_str
 from django.shortcuts import render
+from django.db import connection
 from django.views.generic import ListView
 from .models import Fields, Leads
 
@@ -52,12 +53,13 @@ class FilterView(LeadView, ListView):
     model = Leads
     queryset = Leads.objects.all().values('document')
 
-    def post(self, request):
+    def user_filters(self, request):
         fields = []
         # These 1st IF statements matches up id filter values with 'idstart' and 'idend'
         if request.POST['id_start'] == "":
             idstart = request.POST['id_start']
-        else: idstart = int(request.POST['id_start'])
+        else:
+            idstart = int(request.POST['id_start'])
         if request.POST['id_end'] == "":
             idend = request.POST['id_end']
         else:
@@ -68,93 +70,111 @@ class FilterView(LeadView, ListView):
         #   0 = range: These have min and max values
         #   1 = string:
         #   2 = boolean: A string reading "true", "false", or "both"
-        filters = {'id': [0, idstart, idend]} # The creation of the 'filters' dictionary
+        filters = {'id': [0, idstart, idend]}  # The creation of the 'filters' dictionary
         for key, value in request.POST.items():
-
-            if 'selectfields' in key: # I created some hidden input elements in view.html, whose names are just the rest names with 'selectfields attached. This allowed me to easily get a list of field rest names right here.
+            if 'selectfields' in key:  # I created some hidden input elements in view.html,
+                # whose names are just the rest names with 'selectfields attached.
+                # This allowed me to easily get a list of field rest names right here.
                 fields.append(value)
-            elif '_start' in key and 'id' not in key: # non-date ranges are converted to floatingpoint
-                a = key[:-6] # "start" is removed form the rest name, then value is made into key for 'filters' entry
-                b = float(value) #start value converted from string to float
-                d = 0 # value is datatype variable index. A value of 0 indicates a range variable
+            elif '_start' in key and 'id' not in key:  # non-date ranges are converted to floatingpoint
+                a = key[:-6]  # "start" is removed form the rest name, then value is made into key for 'filters' entry
+                b = float(value)  # start value converted from string to float
+                d = 0  # value is datatype variable index. A value of 0 indicates a range variable
                 for key2, value2 in request.POST.items():
-                    if a +'_end' in key2:
-                        c = float(value2) # The 'end' or max value is converted to float to be added to 'filters'
-                filters.update({a:[d,b,c]}) # non-date and non-id range is added to 'filters'
-            elif 'dstart' in key and 'id' not in key: # date and datetime variables are NOT converted to floats
+                    if a + '_end' in key2:
+                        c = float(value2)  # The 'end' or max value is converted to float to be added to 'filters'
+                filters.update({a: [d, b, c]})  # non-date and non-id range is added to 'filters'
+            elif 'dstart' in key and 'id' not in key:  # date and datetime variables are NOT converted to floats
                 a = key[:-6]
                 b = value
                 d = 0  # value is datatype variable index. A value of 0 indicates a range variable
                 for key2, value2 in request.POST.items():
                     if a + 'dend' in key2:
                         c = value2
-                filters.update({a: [d, b, c]}) # date type is added to 'filters'
+                filters.update({a: [d, b, c]})  # date type is added to 'filters'
             elif '_str' in key:
-                filters.update({key[:-4]: [1,value]}) # the index value of "1" indicates a string variable
+                filters.update({key[:-4]: [1, value]})  # the index value of "1" indicates a string variable
             elif '_boolean' in key:
-                filters.update({key[:-8]: [2,value]}) # the index value of 2 "indicates a boolean variable
+                filters.update({key[:-8]: [2, value]})  # the index value of 2 "indicates a boolean variable
+        return filters
+
+    def filter_leads(self, lead, filters, counter, csvrow, csvdict):
+        for key, value in lead.iteritems():
+            if key == 'document':  # The 'document' JSON is accessed here
+                counter += 1
+                for dockey, docval in value.iteritems():  # Here is where the bulk of the filtering occurs
+                    if docval:  # Some values in 'document' are null, and can't be filtered normally. This automatically disqualifies those leads
+                        for filterkey, filterval in filters.iteritems():
+                            if filterkey == dockey:  # Match keys between 'filters' and 'document'
+                                if filterval[0] == 0:  # range variables
+                                    if (filterval[1] <= docval <= filterval[2]) or (
+                                            filterval[1] == filterval[2] == "") or (
+                                            filterval[1] <= docval and filterval[2] == "") or (
+                                            filterval[1] == "" and filterval[2] >= docval):
+                                        csvrow.update({dockey: docval})
+                                    else:  # if out of range, clear csvrow and kill loop
+                                        csvrow.clear()
+                                        return counter, csvrow, csvdict
+                                elif filterval[0] == 1:  # string variables
+                                    if filterval[1] in docval:
+                                        csvrow.update({dockey: docval})
+                                    else:  # if string does not match, clear csvrow and kill loop
+                                        csvrow.clear(); return counter, csvrow, csvdict
+                                elif filterval[0] == 2:
+                                    if filterval[1] == docval:
+                                        csvrow.update({dockey: docval})
+                                    else:  # if wrong boolean value, clear csvrow and kill loop
+                                        csvrow.clear(); return counter, csvrow, csvdict
+                                else:  # For testing purposes only
+                                    print counter, "INDEX ERROR"
+                                    csvrow.clear()
+                    else:
+                        csvrow.clear(); return counter, csvrow, csvdict # if 'document' value is null
+        # This for loop adds csvrow to csvdict. This only happens if the current lead is not filtered out.
+        for key, value in csvdict.iteritems():
+            for rowkey, rowval in csvrow.iteritems():
+                if key == rowkey:
+                    value.append(rowval)
+        return counter, csvrow, csvdict
+
+    def csv_write(self, csvdict):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="sample.csv"'
+        writer = csv.writer(response)
+        writer.writerow(csvdict.keys())
+        writer.writerows(zip(*csvdict.values()))
+        return response
+
+    def post(self, request):
+        filters = self.user_filters(request)
         #
         # 'filters' is now complete. It is a dictionary where:
         # key = rest name
         # value = list[ datatype, filter_value or min_value, max_value in the case of range variable]
         #
+        # idstr = "Select id, document FROM Leads WHERE (Cast(document->> 'id' AS integer) BETWEEN 500 AND 800)"
+        # emailstr = "AND (document->> 'email' LIKE '%@gmail.com')"
+        # final = idstr + emailstr
+        # example = Leads.objects.raw(final)
+        # for exam in example:
+        #     print exam.id, exam.email
+
         # 'leader' queries the values of the 'document' column in the Leads database
         leader = Leads.objects.all().values('document').values()
         counter = 0 # Goes up by 1 for each lead processed
         csvdict = {} # Dictionary eventually containg the data that will be converted to a CSV
         csvrow = {} # An entry for a single lead that will be added to csvdict
         for filterkey, filterval in filters.iteritems():
-            csvdict.update({filterkey: [""]}) # This just adds the rest names present in 'flters' to the top of csvdict, so they can be the headers for the CSV
+            csvdict.update({filterkey: []}) # This just adds the rest names present in 'filters' to the top of csvdict, so they can be the headers for the CSV
         # Now we iterate through Leads and compare the leads to the filters to see which we keep.
         # If a single lead value is excluded by the filters, the entire lead is dumped and the the next lead is tested
         for lead in leader:
-            loopstatus = 0
             csvrow.clear() # resets csvrow, so new lead data can be added
-            while loopstatus == 0: # Can a lead is disqualified, loopstatus becomes nonzero to break the loop
-                for key, value in lead.iteritems():
-                    if key == 'document': # The 'document' JSON is accessed here
-                        counter += 1
-                        for dockey, docval in value.iteritems(): # Here is where the bulk of the filtering occurs
-                            if docval: # Some values in 'document' are null, and can't be filtered normally. This automatically disqualifies those leads
-                                for filterkey, filterval in filters.iteritems():
-                                    if filterkey == dockey: # Match keys between 'filters' and 'document'
-                                        if filterval[0] == 0: # range variables
-                                            if (filterval[1] <= docval <= filterval[2]) or (filterval[1] == filterval[2] == "") or (filterval[1] <= docval and filterval[2] == "") or (filterval[1] == "" and filterval[2] >= docval):
-                                                csvrow.update({dockey: docval})
-                                            else: # if out of range, clear csvrow and kill loop
-                                                csvrow.clear()
-                                                loopstatus = 1
-                                        elif filterval[0] == 1: # string variables
-                                            if filterval[1] in docval or filterval[1] == "":
-                                                csvrow.update({dockey: docval})
-                                            else: #if string does not match, clear csvrow and kill loop
-                                                csvrow.clear()
-                                                loopstatus = 1
-                                        elif filterval[0] == 2:
-                                            if filterval[1] == docval or (filterval[1] == ""):
-                                                csvrow.update({dockey: docval})
-                                            else: # if wrong boolean value, clear csvrow and kill loop
-                                                csvrow.clear()
-                                                loopstatus = 1
-                                        else: # For testing purposes only
-                                            print counter, "INDEX ERROR"
-                                            csvrow.clear()
-                            else: csvrow.clear(); loopstatus = 1 # if 'document' value is null
-                # This for loop adds csvrow to csvdict. This only happens if the current lead is not filtered out.
-                for key, value in csvdict.iteritems():
-                    for rowkey, rowval in csvrow.iteritems():
-                        if key == rowkey:
-                            value.append(rowval)
-                loopstatus = 1
+            counter, csvrow, csvdict = self.filter_leads(lead, filters, counter, csvrow, csvdict) #filtering of leads takes place here
 
         # csvdict is used to generate a CSV
-        keys = csvdict.keys()
-        sep = b","
-        with open("/Users/kennethharmon/marketing_program_v2/marketing_program_v2/templates/leads/test.csv", "wb") as outfile:
-            writer = csv.writer(outfile, delimiter=sep)
-            writer.writerow(keys)
-            writer.writerows(zip(*[csvdict[key] for key in keys]))
+        response = self.csv_write(csvdict)
 
-        return render(request, "leads/filter.html", {"csvdict": csvdict})
+        return response
 
 
